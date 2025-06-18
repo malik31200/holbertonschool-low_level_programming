e3794a93dca2ed48089471ef379dec4207b151f5
malik31200 <yka-vtc@outlook.fr>
2025-06-18 13:45:01 +0200

    Write a program that prints the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers of base 10 starting from 0
+* Return:always 0
+*/
+
+	int main(void)
+
+{
+	char hpla;
+
+	for (hpla = 122; hpla >= 97; hpla--)
+
+	putchar (hpla);
+	putchar ('\n');
+	return (0);
+}
