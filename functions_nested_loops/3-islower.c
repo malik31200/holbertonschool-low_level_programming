377b16973f335cfdd6aee0fe377a13eafb24e6d3
malik31200 <yka-vtc@outlook.fr>
2025-06-19 20:59:46 +0200

    change vi in 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _islower- function checks for lowercase
- * @c: The character to be checked.
- * Return: 1 if character is lowercase, 0 otherwise
- */
-
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-		return(1)
-	else
-		return(0)
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
 }
