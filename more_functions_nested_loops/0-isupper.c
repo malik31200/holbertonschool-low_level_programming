f227a09709808de75d9cace6ab6d8736f9d08298
malik31200 <yka-vtc@outlook.fr>
2025-06-23 10:27:51 +0200

    function that check for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isupper - Function that check for uppercase character
+ *
+ *Return: 1 if is uppercase 0 otherwise0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		{
+		return (1);
+		}
+		return (0);
+}
