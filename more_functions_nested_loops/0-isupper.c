23a0fbca278f823d6090cfb276fddaf03911a23e
malik31200 <yka-vtc@outlook.fr>
2025-06-23 10:34:33 +0200

    add description

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  *_isupper - Function that check for uppercase character
  *
- *Description - Check for uppercase c is character
+ *@c - c is character
  *Return: 1 if is uppercase 0 otherwise0
  */
 int _isupper(int c)
