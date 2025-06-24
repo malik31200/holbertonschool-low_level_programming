66367b2be85b126ccc86ae8ab8d5e7872becad06
malik31200 <yka-vtc@outlook.fr>
2025-06-24 13:19:58 +0200

    function that takes a pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * @*n : n pointer
+ * @n : is a variable
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
