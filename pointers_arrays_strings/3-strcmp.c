e18ebe2a7d1ac6e70bf2221ca9313db5aa6a2285
malik31200 <yka-vtc@outlook.fr>
2025-06-27 14:55:21 +0200

    change vi

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1)
+	while (s1[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		if (s1[i] < s2[i])
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
-		else
-		{
-			return (s1[i] - s2[i]);
-		}
+	i++;
 	}
 	return (0);
 }
