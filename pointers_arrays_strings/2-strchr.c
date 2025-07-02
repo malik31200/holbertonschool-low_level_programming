d1ab05723efbef778b08bdbda67bf01182c3cabc
malik31200 <yka-vtc@outlook.fr>
2025-07-02 09:15:04 +0200

    change fonction

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,16 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
-	return (NULL);
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
+	}
+	if (c == '\0')
+	{
+		return (s + i);
 	}
 	return (NULL);
 }
