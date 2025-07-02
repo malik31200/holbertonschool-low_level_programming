cc2c2fd34eec43547d8feba78958f6a750d8a7a5
malik31200 <yka-vtc@outlook.fr>
2025-07-02 09:01:46 +0200

    change fonction

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,14 @@
  *_strchr - Function that locates a character in a string
  *@s: is a string
  *@c: is a character
- *@NULL: is valeur to return
  *Return: NULL or s
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-	
+	int i = 0;
+
+	if (s[i] == '\0')
+	return (NULL);
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
