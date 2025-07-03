434c7972f74d2e038cbd3aef238f3a8aa6bd84f9
malik31200 <yka-vtc@outlook.fr>
2025-07-03 13:30:46 +0200

    add all arguments

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Program that multiplies two numbers
+ *@argv: is an array
+ *@argc: is a number of arguments
+ *Return: 0 if success or 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int a = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			a += atoi(argv[i]);
+		}
+	}
+			printf("%d\n", a);
+			return (0);
+}
