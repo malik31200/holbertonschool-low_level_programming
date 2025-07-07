28a1335acd43c5f1073af4110a17b7446a7a5885
malik31200 <yka-vtc@outlook.fr>
2025-07-07 16:43:05 +0200

    check betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,10 +8,11 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
