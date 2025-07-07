c41d5e350749472b8edb78d7e68d575904b8755b
malik31200 <yka-vtc@outlook.fr>
2025-07-07 16:36:48 +0200

    frees a 2 dimens grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid - Frees a 2 dimensional grid
+ *@grid: is an array
+ *@height: is an array
+ *Return: NULL
+ */
+void free_grid(int **grid, int height)
+{
+    int i;
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+    free(grid);
+}
