57f4289cae76c6563a4e9734a98ef0ebbb9b9413
malik31200 <yka-vtc@outlook.fr>
2025-07-09 10:18:14 +0200

    Define a new type struct dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+learn typedef and struct
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - definite a new type
+ * @name: is a name dog
+ * @age: is age dog
+ * @owner: is a owner dog
+ * Return: void
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
