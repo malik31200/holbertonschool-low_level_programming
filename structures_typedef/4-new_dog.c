2f39df1a44957b92f9a59ce68798be3f2ea19fed
malik31200 <yka-vtc@outlook.fr>
2025-07-09 15:41:38 +0200

    add condition on age

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 	{
 		return (NULL);
 	}
