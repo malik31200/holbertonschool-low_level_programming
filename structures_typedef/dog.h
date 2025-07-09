1c462d4194b52ec4796bb96366c38f1e559d962d
malik31200 <yka-vtc@outlook.fr>
2025-07-09 13:18:16 +0200

    add typedef in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: is a owner dog
  * Return: void
  **/
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
