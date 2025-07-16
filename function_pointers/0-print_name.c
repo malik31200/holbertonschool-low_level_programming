e2adcf30b69bcc5e2502c79bd40a70f2030d307b
malik31200 <yka-vtc@outlook.fr>
2025-07-16 09:07:09 +0200

    fix header

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - Function that print a name
@@ -8,10 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
 		f(name);
 }
 
