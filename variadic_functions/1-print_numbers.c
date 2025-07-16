1f82a61ca0e0ebfb9a5305759946d669d97b4310
malik31200 <yka-vtc@outlook.fr>
2025-07-16 16:57:41 +0200

    fix incorrect condition

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,15 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
