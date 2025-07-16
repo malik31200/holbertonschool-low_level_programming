0cfc19d8b6f48352f867dd9dd83e6e8da576a2fe
malik31200 <yka-vtc@outlook.fr>
2025-07-16 16:48:34 +0200

    print the numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - Function that prints number
+ * @separator: is the string to be printed between nbrs
+ * @n: is nbr of integers
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
