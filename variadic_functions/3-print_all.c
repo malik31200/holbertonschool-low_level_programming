7770e5abf9816fa3fb21e08ba53c8cf9007dee9d
malik31200 <yka-vtc@outlook.fr>
2025-07-17 13:06:11 +0200

    fix the prigramm

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,7 +5,6 @@ void print_char(va_list list);
 void print_int(va_list list);
 void print_float(va_list list);
 void print_string(va_list list);
-void(*get_print(char format))(va_list);
 /**
  * print_all - Function that prints anything
  * @format: is a list of types of arguments
@@ -13,20 +12,31 @@ void(*get_print(char format))(va_list);
  */
 void print_all(const char * const format, ...)
 {
-    int i = 0;
-    va_list list;
+	int i = 0, j = 0;
+	va_list list;
+	char format_array[] = {'s', 'i', 'f', 'c', '\0'};
+	void(*ptrf[])(va_list) = {print_string, print_int, print_float, print_char};
+	char *separator = "";
 
+	va_start(list, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (format_array[j] != '\0')
+		{
+			if (format_array[j] == format[i])
+			{
+				printf("%s", separator);
+				ptrf[j](list);
+				separator = ", ";
+			}
 
-    va_start(list, format);
-    while (format[i] != '\0')
-    {
-        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-        {
-                get_print(format[i])(list);
-        }
-        i++;
-    }
-    printf("\n");
+			j++;
+		}
+		i++;
+	}
+	va_end(list);
+	printf("\n");
 }
 
 /**
@@ -36,51 +46,41 @@ void print_all(const char * const format, ...)
  */
 void print_char(va_list list)
 {
-   printf("%c", va_arg(list, int));
+	printf("%c", va_arg(list, int));
 }
 
-/** 
+/**
  * print_int - Print a integer
  * @list: is a list of arguments
  * Return: Nothing
  */
 void print_int(va_list list)
 {
-    printf("%d", va_arg(list, int));
+	printf("%d", va_arg(list, int));
 }
 
-/** 
+/**
  * print_float - Print a integer
  * @list: is a list of arguments
  * Return: Nothing
  */
 void print_float(va_list list)
 {
-    printf("%f", va_arg(list, double));
+	printf("%f", va_arg(list, double));
 }
 
-/** 
+/**
  * print_string - Print a integer
  * @list: is a list of arguments
  * Return: Nothing
  */
 void print_string(va_list list)
 {
-    printf("%s", va_arg(list, char*));
-}
+	char *str = va_arg(list, char*);
 
-/**
- * 
- */
-void(*get_print(char format))(va_list)
-{
-int i = 0;
-
-    char format_array[] = {'s', 'i', 'f', 'c', '\0'};
-    void(*ptrf[])(va_list) = {print_string, print_int, print_float, print_char, NULL};
-    while (format != format_array[i])
-    {
-        i++;
-    }
-    return (ptrf[i]);
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+	printf("%s", str);
 }
