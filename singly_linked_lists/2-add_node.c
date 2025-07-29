35eaf0b46d3f8c71532127ea85952cb2573ea192
malik31200 <yka-vtc@outlook.fr>
2025-07-29 10:38:54 +0200

    add function _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * add_node - Function that add a node at beginnig
  * @head: is a head of the node
@@ -22,9 +23,26 @@ list_t *add_node(list_t **head, const char *str)
 		free(newnode);
 		return (NULL);
 	}
-	newnode->len = strlen(str);
+	newnode->len = _strlen(str);
 	newnode->next = *head;
 	*head = newnode;
 	return (newnode);
 
+}
+/**
+ *_strlen - Calculate Lengh of a string
+ *@str: is a string
+ *Return: len
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
 }
