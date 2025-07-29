ce45942a29cce06e1008c7e854652006acf5c0ec
malik31200 <yka-vtc@outlook.fr>
2025-07-29 09:39:03 +0200

    free a list doubly linked

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *free_list - Frees a doubly list
+ *@head: is a head of elements
+ *Return: NULL
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tofree = head, *temp;
+
+	while (tofree != NULL)
+	{
+		temp = tofree->next;
+		free(tofree);
+		tofree = temp;
+	}
+}
