8b89b271c6024d5aa68931fc6944199262c25033
malik31200 <yka-vtc@outlook.fr>
2025-07-29 12:54:58 +0200

    delete space in description

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,7 @@
  * add_dnodeint - Function that adds a new node at the beginning
  * @head: is a head of the list
  * @n: is an integer
- * Return: new node 
+ * Return: new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
