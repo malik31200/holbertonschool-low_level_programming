7964c0255becfab5a3f384ae8539955c3cdf548f
malik31200 <yka-vtc@outlook.fr>
2025-07-30 16:36:49 +0200

    delete condition

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -27,11 +27,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
-	{
-		len = _strlen(text_content);
-		wcount = write(fd, text_content, len);
-	}
+	len = _strlen(text_content);
+	wcount = write(fd, text_content, len);
 	if (wcount == -1 || wcount != len)
 	{
 		close(fd);
