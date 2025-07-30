61c11c32d867a6b91c46efd84af2be3158937967
malik31200 <yka-vtc@outlook.fr>
2025-07-30 13:29:08 +0200

    fix betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,15 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, count;
 	char *temp;
 
-    if (filename == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
+	if (fd == -1)
+	{
+		return (0);
+	}
 	temp = malloc(letters + 1);
 	if (temp == NULL)
 	{
