84c1facec5078f52d564db3ead36d6511dad9c39
malik31200 <yka-vtc@outlook.fr>
2025-07-30 16:59:34 +0200

    fix return

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		return (-1);
+		return (1);
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
