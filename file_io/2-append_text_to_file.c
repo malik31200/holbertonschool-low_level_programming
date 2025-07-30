c6bd8e0ebe206bc00b8ea263052f1cd4e2196cdf
malik31200 <yka-vtc@outlook.fr>
2025-07-30 16:55:45 +0200

    add condition

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,6 +18,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
