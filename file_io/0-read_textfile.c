acb08a3c85b9912d9a53cb9c39882458df4d40ae
malik31200 <yka-vtc@outlook.fr>
2025-07-30 12:23:07 +0200

    read and print a file to the POSIX

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Function that reads a text file and prints
+ * @filename: is a file
+ * @letters: is the text
+ * Return: 0 is NULL or return the actual number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, count;
+	char *temp;
+
+	fd = open(filename, O_RDONLY);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	temp = malloc(letters + 1);
+	if (temp == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	count = read(fd, temp, letters);
+	if (count == -1)
+	{
+		free(temp);
+		close(fd);
+		return (0);
+	}
+	count = write(1, temp, count);
+	if (count == -1)
+	{
+		free(temp);
+		close(fd);
+		return (0);
+	}
+	free(temp);
+	close(fd);
+	return (count);
+}
