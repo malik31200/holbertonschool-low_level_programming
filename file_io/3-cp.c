749810fbecf891c52be489d930c11738d6b6b11a
malik31200 <yka-vtc@outlook.fr>
2025-07-31 11:44:02 +0200

    add STDERR_FILENO

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,13 +40,13 @@ int cp_file_from(const char *file_from, const char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		close(fd_from);
 		exit(99);
 	}
@@ -55,7 +55,7 @@ int cp_file_from(const char *file_from, const char *file_to)
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written == -1 || n_written != n_read)
 		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -63,12 +63,12 @@ int cp_file_from(const char *file_from, const char *file_to)
 	}
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	close(fd_from);
