4aa5eadaad6a0963ec993a656b01ec9e70fdcab3
malik31200 <yka-vtc@outlook.fr>
2025-07-31 11:26:13 +0200

    fix close

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 		if (argc != 3)
 		{
-			dprintf(2, "Usage: cp file_from file_to\n");
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(97);
 		}
 		res = cp_file_from(argv[1], argv[2]);
@@ -39,22 +39,20 @@ int cp_file_from(const char *file_from, const char *file_to)
 	int fd_from, fd_to, n_read, n_written;
 	char buffer[1024];
 
-	fd_from = open(file_from, O_RDONLY, 0664);
+	fd_from = open(file_from, O_RDONLY);
 
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-
-
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
 		exit(99);
 	}
-
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_written = write(fd_to, buffer, n_read);
@@ -75,5 +73,7 @@ int cp_file_from(const char *file_from, const char *file_to)
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+	close(fd_from);
+	close(fd_to);
 	return (0);
 }
