fb0f36a5235ed6b14f93e10ff6db2215cf451976
malik31200 <yka-vtc@outlook.fr>
2025-07-31 16:02:32 +0200

    function that creates a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Function that creates a hash table
+ * @size: size of an array
+ * @
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,1 @@
+learn hash table
