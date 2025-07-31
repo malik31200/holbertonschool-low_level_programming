418c82afae98d320ca364b10e2f50faa3521d4b2
malik31200 <yka-vtc@outlook.fr>
2025-07-31 21:25:46 +0200

    function that gives the index of a key

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+/**
+ * key_index - Function that gives the index of a key
+ * @key: is a key of value
+ * @size: is the size of a hash table
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
