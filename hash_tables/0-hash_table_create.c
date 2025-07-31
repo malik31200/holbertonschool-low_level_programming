e0a7196741cd27da70c22e7f05f15fdd1ad5a0c3
malik31200 <yka-vtc@outlook.fr>
2025-07-31 16:10:14 +0200

    fix condition

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-	if (ht == NULL)
+	if (ht == NULL || size == 0)
 	{
 		return (NULL);
 	}
