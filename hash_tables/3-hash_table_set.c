8d2d525d070c857ce34951202cafdbd60469100c
malik31200 <yka-vtc@outlook.fr>
2025-08-01 11:23:34 +0200

    deletefile "string.c"

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -50,3 +50,67 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = newnode;
 	return (1);
 }
+/**
+ * _strcmp - Function that compare two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ * Return: 0 is egal or 1 is this not
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (s1[i] - s2[i]);
+}
+/**
+ *  *_strdup - duplicate a string
+ *@str: is a string
+ *Return: if string is NULL return NULL
+ */
+char *_strdup(const char *str)
+{
+	char *copy;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	copy = malloc(sizeof(char) * (i + 1));
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		copy[i] = str[i];
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+/**
+ *_strlen - Returns the lenght of a string
+ *@s: is a pointer
+ *Return: void
+ */
+int _strlen(const char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	return (count);
+}
+
